b709e7eff687c116a3a66d2dd81c5762d060d6c5
András Somi <somiandras@gmail.com>
2022-08-28 23:40:34 +0200

    Fix data path

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from fpl import get_player_data, get_fixtures
 from optimize import optimize
 
-df = get_player_data(my_team_path="../sample_data/team_data.json")
+df = get_player_data(my_team_path="sample_data/team_data.json")
 
 results = optimize(df, 20, 15)
 
