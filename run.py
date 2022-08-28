78ff5ff6899f41995cb4990e8bcba52d007bde83
András Somi <somiandras@gmail.com>
2022-08-28 23:22:17 +0200

    Add runner script and runtime

---
+++ run.py
@@ -0,0 +1,8 @@
+from fpl import get_player_data, get_fixtures
+from optimize import optimize
+
+df = get_player_data(my_team_path="../sample_data/team_data.json")
+
+results = optimize(df, 20, 15)
+
+print(results)
